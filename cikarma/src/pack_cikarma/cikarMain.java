2d9aa820d2d2322cc89e5b1d886ad0bd7b6aeaa7
altrk19 <altrk19@gmail.com>
2017-07-29 09:27:41 +0300

    cikarma eklendi

---
+++ cikarma/src/pack_cikarma/cikarMain.java
@@ -0,0 +1,13 @@
+package pack_cikarma;
+
+public class cikarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sonuc;
+		a=5;b=4;
+		sonuc=a-b;
+		System.out.println(sonuc);
+	}
+
+}
