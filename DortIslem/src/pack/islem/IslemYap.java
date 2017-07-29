7fe1d0665ef6e554c10e67d277f1d81cce3cd3e1
wampraa29 <1306140097@ogr.iu.edu.tr>
2017-07-29 13:17:04 +0300

    Dort işlem projeye eklendi

---
+++ DortIslem/src/pack/islem/IslemYap.java
@@ -0,0 +1,13 @@
+package pack.islem;
+
+public class IslemYap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z;
+		x=5;y=4;z=x+y;
+		System.out.println(z);
+
+	}
+
+}
